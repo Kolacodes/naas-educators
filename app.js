3f1a35ebf86b059b726361eeae29247e74be1120
Bello Kolawole Mustapha <50749200+Kolacodes@users.noreply.github.com>
2021-10-10 18:27:01 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require ("express"),
+    port = process.env.PORT || 5000,
+    bodyParser = require("body-parser");
+
+app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+
+
+app.get("/", function(req, res) {
+    
+    res.render("index");
+});
+
+
+app.get("/xmt", function(req, res) {
+    
+    res.render("xmt");
+});
+
+
+
+
+app.listen(port, function(){
+    console.log("server just started")
+});
